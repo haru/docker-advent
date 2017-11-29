e319d5e5d003da9ae702e8412f537f9acd336c8e
Haruyuki Iida <haru.ii.a9i1e@gmail.com>
2017-11-29 22:36:28 +0900

    change file upload directory

--- run.sh
+++ run.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
+set -e
 
 if [ "$SECRET_KEY_BASE" = "" ]
 then
   export SECRET_KEY_BASE=`rake secret`
 fi
+
+mkdir -p /usr/local/radvent_data/uploads
 
 bundle exec rake db:migrate RAILS_ENV=production
 bundle exec rails s -e production
